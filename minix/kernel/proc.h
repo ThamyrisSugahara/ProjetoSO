7c21b2a286c37276f49aeee9ea0a3a5415dbb4e8
Iago Ayres <135536341+AyresIago@users.noreply.github.com>
2026-05-17 12:08:01 -0300

    Adição da variável p_creation_time
    Essa variável será responsável por armazenar o tempo exato da criação do processo

--- minix/kernel/proc.h
+++ minix/kernel/proc.h
@@ -62,6 +62,8 @@ struct proc {
   clock_t p_virt_left;		/* number of ticks left on virtual timer */
   clock_t p_prof_left;		/* number of ticks left on profile timer */
 
+  clock_t p_creation_time 	/*Variável para armazenar o tempo exato da criação do processo*/
+
   u64_t p_cycles;		/* how many cycles did the process use */
   u64_t p_kcall_cycles;		/* kernel cycles caused by this proc (kcall) */
   u64_t p_kipc_cycles;		/* cycles caused by this proc (ipc) */
